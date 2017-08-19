c412d72e0c9ce6b973c0112b495cdb904d72dd6a
Vlsarro <drekmaro@gmail.com>
2017-08-19 22:51:58 +0300

    Update styling

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import 'rc-slider/assets/index.css';
 import Toggle from 'react-toggle'
 import 'react-toggle/style.css'
 
-
-import logo from './logo.svg';
 import './App.css';
 
 import uniqueId from 'lodash/uniqueId';
+
+
+var resistanceInnerValues = [13.715, 21.625, 31.285, 42.695, 55.865, 70.785,
+						     87.455, 105.885, 126.056, 147.995, 171.685, 197.125],
+	resistanceOuterValues = [9.6005, 15.1375, 21.8995, 29.8865, 39.1055, 49.5495,
+		                     61.2185, 74.1195, 88.2455, 103.5965, 120.1795, 137.9875]
+
+
+function lbToKg (lb) {
+	return lb/2.2046;
+}
+
 
 class SpringNumberSelect extends React.Component {
 	componentWillMount() {
@@ -19,7 +29,6 @@ class SpringNumberSelect extends React.Component {
 
 	render() {
 		const id = this.state.id;
-		console.log(id);
 		const labelStyle = {
 			padding: '5px',
 		    verticalAlign: 'middle',
@@ -32,21 +41,21 @@ class SpringNumberSelect extends React.Component {
 		}
 	    return (
 	    	<div style={divWrapperStyle}>
-		    	<label style={labelStyle} htmlFor={this.id}>Spring number</label>
+		    	<label style={labelStyle} htmlFor={this.id}>Number of springs</label>
 		      	<select id={this.id}>
 				  	<option value="1">1</option>
-				  	<option value="2">2</option>
+				  	<option value="2" disabled>2</option>
+				  	<option value="3" disabled>3</option>
 				</select>
 			</div>
 	    );
 	  }
 }
 
-class WeightConverter extends React.Component {
+class GripToggle extends React.Component {
 
 	render () {
 		let labelStyle = {
-			display: 'inline-block',
 			marginLeft: '20px',
 			verticalAlign: 'middle',
 		    fontWeight: 'normal',
@@ -62,21 +71,27 @@ class WeightConverter extends React.Component {
 
 		return (
 			<label style={labelStyle}>
-	        	<span style={spanStyle}>Kg</span>
+	        	<span style={spanStyle}>Inner</span>
 			  	<Toggle icons={false} />
-			  	<span>Pounds</span>
+			  	<span style={spanStyle}>Outer</span>
 			</label>
 		);
 	}
 }
 
+class Button extends React.Component {
+	render() {
+		return (<input className='resistance-btn' name={this.props.name} type='button' value={this.props.name}/>);
+	}
+}
+
 function log(value) {
-  	console.log(value); //eslint-disable-line
+  	console.log(lbToKg(resistanceInnerValues[value])); //eslint-disable-line
 }
 
 class ResistanceSlider extends React.Component {
 	render () {
-		let divWrapperStyle = { margin: 40 };
+		let divWrapperStyle = { margin: '40px 40px 20px 40px' };
 		return (
 			<div style={divWrapperStyle}>
 				<Slider dots step={1} defaultValue={1} max={11} onAfterChange={log} />
@@ -86,29 +101,24 @@ class ResistanceSlider extends React.Component {
 	}
 }
 
-/*
-<Toggle
-			    defaultChecked={this.state.tofuIsReady}
-			    icons={false}
-			    onChange={this.handleTofuChange} />
- */
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>VICE GRIPPER</h2>
-        </div>
-        <div className="main-content">
-	        <SpringNumberSelect />
-	        <WeightConverter />
-	        <ResistanceSlider />
-        </div>
-      </div>
-    );
-  }
+
+  	render() {
+	    return (
+			<div className="App">
+				<div className="App-header">
+					<h2>VICE GRIPPER</h2>
+				</div>
+				<div className="main-content">
+					<SpringNumberSelect />
+					<GripToggle />
+					<ResistanceSlider />
+					<Button name={'kg'} />
+					<Button name={'lbs'} />
+				</div>
+			</div>
+	    );
+  	}
 }
 
 export default App;
