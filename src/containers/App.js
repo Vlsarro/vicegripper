869db152f0710c9066e77eec15144e6158b4d4ea
Vlsarro <drekmaro@gmail.com>
2017-08-20 11:42:27 +0300

    Fix zero position of slider, indentation

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,9 +13,9 @@ import uniqueId from 'lodash/uniqueId';
 
 
 const resistanceInnerValues = [13.715, 21.625, 31.285, 42.695, 55.865, 70.785,
-						     87.455, 105.885, 126.056, 147.995, 171.685, 197.125],
+							   87.455, 105.885, 126.056, 147.995, 171.685, 197.125],
 	resistanceOuterValues = [9.6005, 15.1375, 21.8995, 29.8865, 39.1055, 49.5495,
-		                     61.2185, 74.1195, 88.2455, 103.5965, 120.1795, 137.9875];
+							 61.2185, 74.1195, 88.2455, 103.5965, 120.1795, 137.9875];
 
 const INNER = false;
 
@@ -33,8 +33,8 @@ class SpringNumberSelect extends React.Component {
 	}
 
 	componentWillMount() {
-	    const id = uniqueId("prefix-");
-	    this.setState({id: id});
+		const id = uniqueId("prefix-");
+		this.setState({id: id});
 	}
 
 	onChange(e) {
@@ -44,25 +44,25 @@ class SpringNumberSelect extends React.Component {
 	render() {
 		const labelStyle = {
 			padding: '5px',
-		    verticalAlign: 'middle',
-		    fontWeight: 'normal',
-		    marginBottom: '0'
+			verticalAlign: 'middle',
+			fontWeight: 'normal',
+			marginBottom: '0'
 		};
 
 		const divWrapperStyle = {
 			display: 'inline-block'
 		}
-	    return (
-	    	<div style={divWrapperStyle}>
-		    	<label style={labelStyle} htmlFor={this.id}>Number of springs</label>
-		      	<select id={this.id} onChange={this.onChange}>
-				  	<option value="1">1</option>
-				  	<option value="2" disabled>2</option>
-				  	<option value="3" disabled>3</option>
+		return (
+			<div style={divWrapperStyle}>
+				<label style={labelStyle} htmlFor={this.id}>Number of springs</label>
+				<select id={this.id} onChange={this.onChange}>
+					<option value="1">1</option>
+					<option value="2" disabled>2</option>
+					<option value="3" disabled>3</option>
 				</select>
 			</div>
-	    );
-	  }
+		);
+	}
 }
 
 class GripToggle extends React.Component {
@@ -80,22 +80,22 @@ class GripToggle extends React.Component {
 		let labelStyle = {
 			marginLeft: '20px',
 			verticalAlign: 'middle',
-		    fontWeight: 'normal',
-		    marginBottom: '0'
+			fontWeight: 'normal',
+			marginBottom: '0'
 		};
 
 		const spanStyle = {
 			padding: '5px',
 			verticalAlign: 'middle',
-		    fontWeight: 'normal',
-		    marginBottom: '0'
+			fontWeight: 'normal',
+			marginBottom: '0'
 		}
 
 		return (
 			<label style={labelStyle}>
-	        	<span style={spanStyle}>Inner</span>
-			  	<Toggle icons={false}  onChange={this.onChange} />
-			  	<span style={spanStyle}>Outer</span>
+				<span style={spanStyle}>Inner</span>
+				<Toggle icons={false}  onChange={this.onChange} />
+				<span style={spanStyle}>Outer</span>
 			</label>
 		);
 	}
@@ -108,7 +108,6 @@ class ResistanceSlider extends React.Component {
 	}
 
 	onAfterChange(value) {
-		
 		this.props.onAfterChange(value);
 	}
 
@@ -116,7 +115,7 @@ class ResistanceSlider extends React.Component {
 		let divWrapperStyle = { margin: '40px 40px 20px 40px' };
 		return (
 			<div style={divWrapperStyle}>
-				<Slider dots step={1} defaultValue={1} max={11} onAfterChange={this.onAfterChange} />
+				<Slider dots step={1} defaultValue={this.props.defaultPosition} max={11} onAfterChange={this.onAfterChange} />
 				<p>{this.props.resistance + ' ' + this.props.weightUnit}</p>
 			</div>
 		);
@@ -134,22 +133,24 @@ class App extends Component {
 		this.onAfterChange = this.onAfterChange.bind(this);
 		this.updateResistance = this.updateResistance.bind(this);
 
+		const initialSliderPosition = 1;
+
 		this.state = {
 			weightUnit: 'lbs',
 			grip: INNER,
 			springNumber: 1,
-			resistance: resistanceInnerValues[1],
-			resistanceSliderPosition: 1
+			resistance: resistanceInnerValues[initialSliderPosition],
+			sliderPosition: initialSliderPosition
 		};
 	}
 
-	updateResistance(newResistance) {
+	updateResistance(newPosition) {
 		let stateObject, value;
-		if (newResistance) {
-			value = this.state.grip === INNER ? resistanceInnerValues[newResistance] : resistanceOuterValues[newResistance];
-			stateObject = {resistanceSliderPosition: newResistance, resistance: this.state.weightUnit === 'kg' ? lbToKg(value) : value};
+		if (newPosition !== undefined) {
+			value = this.state.grip === INNER ? resistanceInnerValues[newPosition] : resistanceOuterValues[newPosition];
+			stateObject = {sliderPosition: newPosition, resistance: this.state.weightUnit === 'kg' ? lbToKg(value) : value};
 		} else {
-			value = this.state.grip === INNER ? resistanceInnerValues[this.state.resistanceSliderPosition] : resistanceOuterValues[this.state.resistanceSliderPosition];
+			value = this.state.grip === INNER ? resistanceInnerValues[this.state.sliderPosition] : resistanceOuterValues[this.state.sliderPosition];
 			stateObject = {resistance: this.state.weightUnit === 'kg' ? lbToKg(value) : value};
 		}
 		this.setState(stateObject);
@@ -167,12 +168,12 @@ class App extends Component {
 		this.setState({weightUnit: newWeightUnit}, this.updateResistance);
 	}
 
-	onAfterChange(newResistance) {
-		this.updateResistance(newResistance);
+	onAfterChange(newPosition) {
+		this.updateResistance(newPosition);
 	}
 
-  	render() {
-	    return (
+	render() {
+		return (
 			<div className="App">
 				<div className="App-header">
 					<h2>VICE GRIPPER</h2>
@@ -180,13 +181,14 @@ class App extends Component {
 				<div className="main-content">
 					<SpringNumberSelect initialValue={this.state.springNumber} onSpringNumberChange={this.onSpringNumberChange}  />
 					<GripToggle initialGrip={this.state.grip} onGripChange={this.onGripChange} />
-					<ResistanceSlider resistance={this.state.resistance} weightUnit={this.state.weightUnit} grip={this.state.grip} onAfterChange={this.onAfterChange}  />
+					<ResistanceSlider resistance={this.state.resistance} defaultPosition={this.state.sliderPosition}
+										weightUnit={this.state.weightUnit} grip={this.state.grip} onAfterChange={this.onAfterChange}  />
 					<Button name={'kg'} onButtonClick={this.onButtonClick} />
 					<Button name={'lbs'} onButtonClick={this.onButtonClick} />
 				</div>
 			</div>
-	    );
-  	}
+		);
+	}
 }
 
 export default App;
