57f221db25e9149dca17f651036b7a09bbea44b5
Vlsarro <drekmaro@gmail.com>
2021-05-02 08:56:07 +0300

    update app render

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './utils/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 registerServiceWorker();
